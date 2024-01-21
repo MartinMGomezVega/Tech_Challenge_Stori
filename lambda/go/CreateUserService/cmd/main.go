ac9c6ea72177d13bc135435061d71d85bf173c4f
MartinMGomezVega <martin_gomezvega@hotmail.com>
2024-01-21 12:41:36 -0300

    Actualizado

--- lambda/go/CreateUserService/cmd/main.go
+++ lambda/go/CreateUserService/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/MartinMGomezVega/Tech_Challenge_Stori/lambda/go/CreateUserService/internal/processor"
 	"github.com/MartinMGomezVega/Tech_Challenge_Stori/lambda/go/CreateUserService/internal/repository"
 	"github.com/MartinMGomezVega/Tech_Challenge_Stori/lambda/go/CreateUserService/pkg/handler"
@@ -8,8 +10,18 @@ import (
 )
 
 func main() {
+	fmt.Println("Start main")
 	r := repository.NewCreateUser()
+
+	fmt.Println("new processor")
 	p := processor.New(r)
+
+	fmt.Println("new handler")
 	handler := handler.New(p)
+
+	fmt.Println("Start handler")
 	lambda.Start(handler)
+
+	fmt.Println("Start main")
+
 }
