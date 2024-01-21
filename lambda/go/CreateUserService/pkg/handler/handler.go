b6306a163f7c3dc6a3f84083bfd24ab8f40c42b1
MartinMGomezVega <martin_gomezvega@hotmail.com>
2024-01-21 12:34:56 -0300

    Actualizado

--- lambda/go/CreateUserService/pkg/handler/handler.go
+++ lambda/go/CreateUserService/pkg/handler/handler.go
@@ -17,11 +17,11 @@ func New(p *processor.Processor) *Handler {
 	return &Handler{processor: p}
 }
 
-func (h *Handler) HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) *events.APIGatewayProxyResponse {
+func (h *Handler) HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 
 	fmt.Print("<start> <handler> <HandleRequest> Creating the user...")
 
 	res := h.processor.Process(ctx, request)
 
-	return res
+	return res, nil
 }
--- lambda/go/CreateUserService/subo.bat
+++ lambda/go/CreateUserService/subo.bat
@@ -3,6 +3,8 @@ git commit -m "Actualizado"
 git push
 set GOOS=linux
 set GOARCH=amd64
+cd cmd
 go build main.go
+cd ..
 del main.zip
 tar.exe -a -cf main.zip main
